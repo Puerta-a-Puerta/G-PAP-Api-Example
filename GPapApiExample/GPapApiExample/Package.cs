a6baf774220d8e76a075d65d6965fe637dc92ccd
djhvscf <dennishernandezvargas@gmail.com>
2018-11-19 08:19:51 -0600

    .

---
+++ GPapApiExample/GPapApiExample/Package.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPapApiExample
+{
+    public class Package
+    {
+        public string ClientName { get; set; }
+        public string Phone { get; set; }
+        public string Phone2 { get; set; }
+        public string MethodPayment { get; set; }
+        public string Provincia { get; set; }
+        public string Canton { get; set; }
+        public string Distrito { get; set; }
+        public string Barrio { get; set; }
+        public string Address { get; set; }
+        public string Address2 { get; set; }
+        public string AddressDelivery { get; set; }
+        public string AddressDelivery2 { get; set; }
+        public string Product { get; set; }
+        public int Cantidad { get; set; }
+        public double Quantity { get; set; }
+        public string Transport { get; set; }
+        public double LittlePackages { get; set; }
+        public double MediumPackages { get; set; }
+        public double BigPackages { get; set; }
+        public string Comments { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+        public string LongitudeDelivery { get; set; }
+        public string LatitudeDelivery { get; set; }
+        public string FechaRuta { get; set; }
+        public bool PaidOrigin { get; set; }
+        public string Bill { get; set; }
+        public bool ToGAM { get; set; }
+        public string PhoneDelivery { get; set; }
+        public string PhoneDelivery2 { get; set; }
+        public string ProvinciaDelivery { get; set; }
+        public string CantonDelivery { get; set; }
+        public string DistritoDelivery { get; set; }
+    }
+}
--- GPapApiExample/GPapApiExample/Program.cs
+++ GPapApiExample/GPapApiExample/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +12,7 @@ namespace GPapApiExample
     class Program
     {
         private static HttpClient _httpClient;
-        private const string REST_URL = "http://166.62.126.46:8080/{0}";
+        private const string REST_URL = "http://166.62.126.46:8082/{0}";
 
         static void Main(string[] args)
         {
@@ -17,7 +20,8 @@ namespace GPapApiExample
 
             if (user != null)
             {
-                GetMyPackagesAsync(user.accessToken).Wait();
+                CreatePackage(user.accessToken);
+                //GetMyPackagesAsync(user.accessToken).Wait();
             }
         }
 
@@ -30,8 +34,8 @@ namespace GPapApiExample
             User user = null;
             try
             {
-                string username = "yourusername";
-                string password = "yourpassword";
+                string username = "interkontakt";
+                string password = "interkontakt2018*";
 
                 var uri = new Uri(string.Format(REST_URL, "api/User/login"));
                 var jsonRequest = JsonConvert.SerializeObject(new { Username = username, Password = password });
@@ -49,8 +53,8 @@ namespace GPapApiExample
 
                 //if (response.IsSuccessStatusCode)
                 //{
-                    var content = await response.Content.ReadAsStringAsync();
-                    user = JsonConvert.DeserializeObject<User>(content);
+                var content = await response.Content.ReadAsStringAsync();
+                user = JsonConvert.DeserializeObject<User>(content);
                 //}
             }
             catch (Exception ex)
@@ -59,6 +63,81 @@ namespace GPapApiExample
             }
 
             return user;
+        }
+
+        private static async void CreatePackage(string token)
+        {
+            Package pack = new Package()
+            {
+                Address = "CALLE 32 con AV 2, DIAGONAL A LA EMBAJADA DE ESPAÑA. Edf. Alefbet",
+                Address2 = string.Empty,
+                AddressDelivery = "12345, test, test, TEST, test, 1, 1, 1",
+                AddressDelivery2 = string.Empty,
+                Barrio = "San Bosco",
+                BigPackages = 0,
+                Cantidad = 1,
+                Canton = "San José Centro",
+                CantonDelivery = "TEST",
+                ClientName = "test test test",
+                Comments = "costa test",
+                Distrito = "Mata Redonda",
+                DistritoDelivery = "TEST",
+                FechaRuta = "20-11-2018",
+                Latitude = string.Empty,
+                LatitudeDelivery = string.Empty,
+                LittlePackages = 1.0,
+                Longitude = string.Empty,
+                LongitudeDelivery = string.Empty,
+                MediumPackages = 0,
+                MethodPayment = "Cash",
+                PaidOrigin = false,
+                Phone = "1234567890",
+                Phone2 = string.Empty,
+                PhoneDelivery = "25691811",
+                PhoneDelivery2 = string.Empty,
+                Product = "costa test:1",
+                Provincia = "test",
+                ProvinciaDelivery = "TEST",
+                Quantity = 49.0,
+                ToGAM = true,
+                Transport = "Motorcycle",                
+            };
+
+            try
+            {
+
+
+                var client = new RestClient("http://166.62.126.46:8082");
+                var request = new RestRequest("api/Package/Create", Method.POST);
+                request.RequestFormat = DataFormat.Json;
+                request.AddBody(pack);
+                request.AddHeader("access-token", token);
+
+                var response = client.Execute(request);
+                var obj = JObject.Parse(response.Content);
+
+                /*var uri = new Uri(string.Format(REST_URL, "api/Package/Create"));
+
+                HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, uri);
+
+                requestMessage.Headers.Add("access-token", token);
+
+                if (_httpClient == null)
+                {
+                    _httpClient = new HttpClient();
+                }
+
+                var response = await _httpClient.PostAsync("api/Package/Create", null);
+
+                //if (response.IsSuccessStatusCode)
+                //{
+                var content = await response.Content.ReadAsStringAsync();
+                //}*/
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         /// <summary>
@@ -73,7 +152,7 @@ namespace GPapApiExample
                 var uri = new Uri(string.Format(REST_URL, "api/Package/getPackages"));
 
                 HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
-    
+
                 requestMessage.Headers.Add("access-token", token);
 
                 if (_httpClient == null)
